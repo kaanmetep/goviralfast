5cf6f40276c799e24b1ca2aa333d45a2107cb615
kaanmetep <kaanpmete@gmail.com>
2025-02-28 00:04:14 +0300

    fix searchparams with adding suspense

--- components/LoginPopup.jsx
+++ components/LoginPopup.jsx
@@ -1,53 +1,11 @@
-"use client";
-import LoginPopupSignIn from "./LoginPopupSignIn";
-import LoginPopupSignUp from "./LoginPopupSignUp";
-import { X } from "lucide-react";
-import { useAppContext } from "@/context/AppContext";
-import { signInAction } from "@/actions";
-import { useTransition, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import LoginPopupContent from "./LoginPopupContent";
 
 const LoginPopup = () => {
-  const searchParams = useSearchParams();
-  const [isPending, startTransition] = useTransition();
-  const handleSignIn = () => {
-    startTransition(async () => {
-      await signInAction();
-    });
-  };
-  const { selectedOption, setSelectedOption } = useAppContext();
-  useEffect(() => {
-    if (searchParams.get("showLogin") === "true") {
-      setSelectedOption("signin");
-    }
-  }, [searchParams]);
-  if (selectedOption === null) return;
-
   return (
-    <div className=" fixed top-0  inset-0 h-full backdrop-blur-md z-[1000] flex items-center justify-center px-3 ">
-      <div className="relative bg-white w-[800px] flex rounded-md border-yellow-50 border">
-        {/* SIGN UP */}
-        <LoginPopupSignUp
-          selectedOption={selectedOption}
-          handleSignIn={handleSignIn}
-          isPending={isPending}
-          setSelectedOption={setSelectedOption}
-        />
-        {/* SIGN IN */}
-        <LoginPopupSignIn
-          selectedOption={selectedOption}
-          handleSignIn={handleSignIn}
-          isPending={isPending}
-          setSelectedOption={setSelectedOption}
-        />
-        <button
-          className="absolute right-3 top-3"
-          onClick={() => setSelectedOption(null)}
-        >
-          <X />
-        </button>
-      </div>
-    </div>
+    <Suspense>
+      <LoginPopupContent />
+    </Suspense>
   );
 };
 
