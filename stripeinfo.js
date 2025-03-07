ea9e1cb548fe7f38267a29a67c1e7af40515ab1e
kaanmetep <kaanpmete@gmail.com>
2025-03-07 12:54:43 +0300

    fix stripe link for production too

--- stripeinfo.js
+++ stripeinfo.js
@@ -2,9 +2,11 @@ export const monthlyPlan = {
   link:
     process.env.NODE_ENV === "development"
       ? "https://buy.stripe.com/test_bIY8xA1yTgz7bQI6or"
-      : "",
+      : "https://buy.stripe.com/test_bIY8xA1yTgz7bQI6or",
   priceId:
     process.env.NODE_ENV === "development"
       ? "price_1QzIiMGK2mnl1LsASbTiv5Cy"
-      : "",
+      : "price_1QzIiMGK2mnl1LsASbTiv5Cy",
 };
+
+// change these links later
