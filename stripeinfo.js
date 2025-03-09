9071f50cbfeef1c3750b95e23e94c21c6870e4c6
kaanmetep <kaanpmete@gmail.com>
2025-03-10 00:19:49 +0300

    fix stripe

--- stripeinfo.js
+++ stripeinfo.js
@@ -2,11 +2,11 @@ export const monthlyPlan = {
   link:
     process.env.NODE_ENV === "development"
       ? "https://buy.stripe.com/test_bIY8xA1yTgz7bQI6or"
-      : "https://buy.stripe.com/test_bIY8xA1yTgz7bQI6or",
+      : "https://buy.stripe.com/28o4gn9kZ3lT29q4gg",
   priceId:
     process.env.NODE_ENV === "development"
       ? "price_1QzIiMGK2mnl1LsASbTiv5Cy"
-      : "price_1QzIiMGK2mnl1LsASbTiv5Cy",
+      : "price_1R0r3FGK2mnl1LsAJNpG7EwL",
 };
 
 // change these links later
